140560aed8e9a94e2fbe0bffc2481aba3c8a1602
Marc <marcvalentinesmiro@gmail.com>
2026-01-08 19:20:19 +0100

    Fix error login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\CheckUserActive;
 use Illuminate\Support\Facades\Route;
 
 /*/
